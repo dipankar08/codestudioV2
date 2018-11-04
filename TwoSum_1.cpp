89fcd7fba0cf399a12f4bb3030b85b6fbab4ed8e
Dipankar Dutta <dutta.dipankar08@gmail.com>
2018-11-04 18:50:25 +0000

    Update TwoSum_1.cpp

--- TwoSum_1.cpp
+++ TwoSum_1.cpp
@@ -6,7 +6,7 @@ void twoSum(vector<int> arr,int K) {
   for (int i = 0; i < arr.size(); i++) { 
     for (int j = i + 1; j < arr.size(); j++) {
       if (arr[i] + arr[j] == K) {
-        cout<<"Found :"<<arr[i]<<" + "<<arr[j]<<": "+K;
+        cout<<"Found :"<<arr[i]<<" + "<<arr[j]<<": "+K<<endl;
       }
     }
   }
@@ -14,5 +14,6 @@ void twoSum(vector<int> arr,int K) {
 
 int main()
 {
+    twoSum({3,2,4,1},5);
     twoSum({3,2,4,1},10);
 }
